db0bdd1cd4db25f19bc9a05123e7d68dd1f5c0e2
Alexander Hoybia <c1747-23@itmlyceumalabang.onmicrosoft.com>
2025-08-30 13:03:01 +0800

    Added Database Connection String

---
+++ Library Management/Data/BookDbContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Library_Management.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Management.Data;
+
+public partial class BookDbContext : DbContext
+{
+    public BookDbContext()
+    {
+    }
+
+    public BookDbContext(DbContextOptions<BookDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    public virtual DbSet<BookCopy> BookCopies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=LibraryDB;Trusted_Connection=True;TrustServerCertificate=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Author__3214EC07AAEC0E9B");
+
+            entity.ToTable("Author");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.BirthDate).HasColumnType("datetime");
+            entity.Property(e => e.Name).HasMaxLength(255);
+            entity.Property(e => e.ProfileImageUrl).HasMaxLength(500);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Book__3214EC07964B10DB");
+
+            entity.ToTable("Book");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Genre).HasMaxLength(100);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.PublishedDate).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<BookCopy>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__BookCopy__3214EC07BB9CE1AC");
+
+            entity.ToTable("BookCopy");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.AddedDate).HasColumnType("datetime");
+            entity.Property(e => e.Condition).HasMaxLength(100);
+            entity.Property(e => e.CoverImageUrl).HasMaxLength(500);
+            entity.Property(e => e.PulloutDate).HasColumnType("datetime");
+            entity.Property(e => e.PulloutReason).HasMaxLength(255);
+            entity.Property(e => e.Source).HasMaxLength(100);
+
+            entity.HasOne(d => d.Book).WithMany(p => p.BookCopies)
+                .HasForeignKey(d => d.BookId)
+                .HasConstraintName("FK__BookCopy__BookId__2B3F6F97");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
